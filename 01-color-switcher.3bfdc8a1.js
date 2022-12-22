d41ea50e55f666a3381c1d03ae2364622499e52a
PanSerg <108748956+PanSerg@users.noreply.github.com>
2022-12-22 18:46:05 +0000

    Deploying to gh-pages from @ PanSerg/goit-js-hw-09@5fd7869731c3cbbde4a0d7e4663ddbef37f771e2 🚀

---
+++ 01-color-switcher.3bfdc8a1.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector(".body");var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;t.addEventListener("click",(function(){n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.3bfdc8a1.js.map
---
+++ 01-color-switcher.fbda1c9a.js
@@ -0,0 +1,2 @@
+document.querySelector(".body");const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let o=null;t.addEventListener("click",(function(){o=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.fbda1c9a.js.map
