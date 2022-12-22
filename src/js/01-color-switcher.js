5fd7869731c3cbbde4a0d7e4663ddbef37f771e2
Sergey Opanasenko <svopanasenko22@gmail.com>
2022-12-22 19:45:27 +0100

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,6 @@
 const body = document.querySelector('.body');
-const startBtn = document.querySelector('button[data-start = "start"]');
-const stopBtn = document.querySelector('button[data-stop = "stop"]');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
 const INTERVAL_CHANGE = 1000;
 let intervalId = null;
@@ -8,18 +8,21 @@ let intervalId = null;
 startBtn.addEventListener('click', changeColor);
 stopBtn.addEventListener('click', stopColor);
 
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 function changeColor() {
-    intervalId = setTimeout(() => {
-        console.log('Изменить цвет');
+    intervalId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
     }, INTERVAL_CHANGE);
+    startBtn.disabled = true;
 }
 
 function stopColor() {
     clearInterval(intervalId);
+    startBtn.disabled = false;
 }
 
 
