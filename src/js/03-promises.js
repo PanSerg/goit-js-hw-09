83d44de8a4d01114e89f4d64941b116be4842dd6
Sergey Opanasenko <svopanasenko22@gmail.com>
2022-12-26 00:26:27 +0100

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,32 @@
+const container = document.querySelector('.form');
+
+const delayRef = document.querySelector('.delay');
+const stepRef = document.querySelector('.step');
+const amountRef = document.querySelector('.amount');
+const start = document.querySelector('[type="submit"]');
+
+start.addEventListener('click', onClick);
+
+function onClick() {
+  for (let i = 0; i < container.children.length; i += 1) {
+     createPromise(position, delay, i * 500).then(position =>
+       console.log(position)
+     );
+   }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+
+ 
+  setTimeout(() => {
+    if (shouldResolve) {
+      position('Промис выполнен');
+    } else {
+      delay('Промис выпиолнен с ошибкой');
+    }
+  }, delay);
+  return shouldResolve;
+};
+
+  console.log(createPromise);
